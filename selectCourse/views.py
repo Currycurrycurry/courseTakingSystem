b82e2fa1c9386cf4d7e23b387f8a60f3913d03da
Currycurrycurry <17302010063@fudan.edu.cn>
2019-12-09 08:19:20 +0800

    organize the code structure

--- selectCourse/views.py
+++ selectCourse/views.py
@@ -7,6 +7,7 @@ from django.db import connection
 import json
 import xlrd
 
+##################################Constants#################################################
 # AUTHORIZATION
 ROOT_ROLE = 0
 STUDENT_ROLE = 1
@@ -17,48 +18,18 @@ STATUS_PENDING = 0 # submitted successfully
 STATUS_PASSED = 1
 STATUS_UNPASSED = -1
 
+# EXCEL FILE 
 STUDENT_FILE = 1
 COURSE_FILE = 2
 SCORE_FILE = 3
 
 
+##################################Index#################################################
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def login(request):
-    res = {
-        'code': 0,
-        'msg': '',
-        'data': {}
-    }
-    if request.method == "POST":
-        data = request.POST
-        print(data)
-        print(models.Account.objects.all())
-        user_id = data.get('id')
-        pwd = data.get('password')
-
-        try:
-            user = models.Account.objects.get(id=user_id)
-            if user.password == pwd:
-
-                student = models.Student.objects.get(student_id=user_id)
-                # other required values to be added in the session
-                request.session['is_login'] = True
-                request.session['user_name'] = student.student_name
-                request.session['role'] = user.role
-                request.session.set_expiry(0)
-
-                res['msg'] = 'login successfully'
-                res['code'] = 1
-                res['data'] = {'user_id': user_id}
-            else:
-                res['msg'] = 'wrong password'
-        except:
-            res['msg'] = 'wrong userid'
-        
-        return HttpResponse(json.dumps(res),content_type = 'application/json')
+##################################Login in/out#################################################
 
 def login_sql(request):
     res = {
@@ -121,58 +92,9 @@ def logout(request):
     request.session.flush()
     pass
 
-# PAGE 2: for students to select and drop courses (2&4 can be merged to one page)
-def selectCourse(request):
-    res = {
-        'code': 0,
-        'msg': ''
-    }
-    # 0 - root 1 - students 2 -teachers
-    if request.session['is_login'] == True and request.session['role'] == STUDENT_ROLE:
-        user_id = request.GET['user_id']
-        course_id = request.GET['course_id']
-        section_id = request.GET['section_id']
-        print("the course id is ",course_id)
-        print("the section id is ",section_id)
-
-        try:
-            course = models.Course.objects.get(course_id=course_id)
-            print("course is ",course)
-            section = models.Section.objects.get(course=course)
-            print("section is ",section)
-            student = models.Student.objects.get(student_id=user_id)
-            print("student is ",student)
-            take_num = models.Takes.objects.filter(course_id=course_id).count()
-            print(take_num)
-            # models.Takes.objects.create(course_id="XDSY118019",section_id=1,student=student,drop_flag=1)
-
-            # not quite sure if student=student is ok
-            if models.Takes.objects.filter(course_id=course_id,student=student):
-                res['msg'] = 'already selected'
-            elif take_num < section.limit:
-                pass
-                # bug1: since the drop_flag is a NOT NULL integer field, it must be defined in the create sentence!!!
-                #  INSERT INTO "takes" ("course_id", "section_id", "student_id", "grade", "drop_flag") SELECT 'XDSY118020', 1, '17302010015', NULL, NULL; args=('XDSY118020', 1, '17302010015', None, None)
-                models.Takes.objects.create(course_id=course.course_id,section_id=section.section_id,student=student,drop_flag=0)
-                # models.Student.objects.filter(student_id=user_id).update(student_total_credit=student_total_credit+course.credits) # buggy: the r_val is not defined
-                # use the save() method to update the credit
-                student.student_total_credit += course.credits
-                student.save()
-                res['msg'] = 'select successfully'
-                res['code'] = 1
-            else:
-                res['msg'] = 'course with no vacancy'
-            
-        except Exception as e:
-            res['msg'] = 'wrong course id'
-            print(str(e))
-    else: 
-        res['msg'] = 'unauthorized as student'
-
-    return HttpResponse(json.dumps(res),content_type = 'application/json')
-
+##################################Select#################################################
 # TODO 选课时间冲突检测 
-# TODO 考试时间冲突检查？？
+# TODO 考试时间冲突检查
 def select_sql(request):
     res = {
         'code': 0,
@@ -246,50 +168,7 @@ def select_sql(request):
 
     return HttpResponse(json.dumps(res),content_type = 'application/json')
 
-
-def dropCourse(request):
-    res = {
-        'code': 0,
-        'msg': ''
-    }
-    # 0 - root 1 - students 2 -teachers
-    if request.session['is_login'] == True and request.session['role'] == STUDENT_ROLE:
-        user_id = request.GET['user_id']
-        course_id = request.GET['course_id']
-        print("the course id is ",course_id)
-
-        try:
-            # SELECT "course"."course_id", "course"."title", "course"."credits" FROM "course" WHERE "course"."course_id" = 'XDSY118019'; args=('XDSY118019',)
-            course = models.Course.objects.get(course_id=course_id)
-            # SELECT "section"."course_id", "section"."section_id", "section"."time", "section"."classroom_no", "section"."lesson", "section"."limit", "section"."day" FROM "section" WHERE "section"."course_id" = 'XDSY118019'; args=('XDSY118019',)
-            section = models.Section.objects.get(course=course)
-            # SELECT "student"."student_id", "student"."student_name", "student"."student_major", "student"."student_dept_name", "student"."student_total_credit" FROM "student" WHERE "student"."student_id" = '17302010015'; args=('17302010015',)
-            student = models.Student.objects.get(student_id=user_id)
-            # SELECT COUNT(*) AS "__count" FROM "takes" WHERE "takes"."course_id" = 'XDSY118019'; args=('XDSY118019',)
-            take_num = models.Takes.objects.filter(course_id=course_id).count()
-            # not quite sure if student=student is ok
-            # SELECT "takes"."course_id", "takes"."section_id", "takes"."student_id", "takes"."grade", "takes"."drop_flag" FROM "takes" WHERE ("takes"."course_id" = 'XDSY118019' AND "takes"."student_id" = '17302010015'); args=('XDSY118019', '17302010015')
-            if models.Takes.objects.filter(course_id=course_id,student=student):
-
-                # DELETE FROM "takes" WHERE ("takes"."course_id" = 'XDSY118019' AND "takes"."student_id" = '17302010015'); args=('XDSY118019', '17302010015')
-                models.Takes.objects.filter(course_id=course_id,student=student).delete()
-
-                # models.Student.objects.filter(student_id=user_id).update(student_total_credit=student_total_credit-course.credits) # maybe buggy
-                # UPDATE "student" SET "student_name" = '黄鼎竣', "student_major" = '软件工程', "student_dept_name" = '软件学院', "student_total_credit" = -2 WHERE "student"."student_id" = '17302010015'; args=('黄鼎竣', '软件工程', '软件学院', -2, '17302010015')
-                student.student_total_credit-=course.credits
-                student.save()
-                res['msg'] = 'drop successfully'
-                res['code'] = 1
-
-            else:
-                res['msg'] = 'drop error: haven\'t taken yet' 
-        except:
-            res['msg'] = 'wrong course id'
-    else: 
-        res['msg'] = 'unauthorized as student'
-
-    return HttpResponse(json.dumps(res),content_type = 'application/json')
-
+##################################Drop#################################################
 # TODO if drop the course applied before, set the drop_flag = 1 
 def dropCourse_sql(request): 
     res = {
@@ -349,40 +228,8 @@ def dropCourse_sql(request):
 
     return HttpResponse(json.dumps(res),content_type = 'application/json')
 
-# PAGE 4: for students to check all his/her courses and the school courses
-def checkCourseTable(request):
-    res = {
-        'code': 0,
-        'msg': '',
-        'data':{}
-    }
-    # 0 - root 1 - students 2 -teachers
-    if request.session['is_login'] == True and request.session['role'] == STUDENT_ROLE:
-        user_id = request.GET['user_id']
-        print("the user id is ",user_id)
-        # SELECT "student"."student_id", "student"."student_name", "student"."student_major", "student"."student_dept_name", "student"."student_total_credit" FROM "student" WHERE "student"."student_id" = '17302010015'; args=('17302010015',)
-        student = models.Student.objects.get(student_id=user_id)
-        # SELECT "takes"."course_id", "takes"."section_id", "takes"."student_id", "takes"."grade", "takes"."drop_flag" FROM "takes" WHERE "takes"."student_id" = '17302010015'; args=('17302010015',)
-        courses = models.Takes.objects.filter(student=student)
-        # iterate the courses in the section table
-        data = []
-        for course in courses:
-            # SELECT "section"."course_id", "section"."section_id", "section"."time", "section"."classroom_no", "section"."lesson", "section"."limit", "section"."day" FROM "section" WHERE "section"."course_id" = 'XDSY118020'; args=('XDSY118020',)
-            # the course name and credit hasn't been showed since the model may be changed later
-            data.append(serializers.serialize('python',models.Section.objects.filter(course=course.course_id)))
-        # data = serializers.serialize('python',models.Takes.objects.filter(student=student))
-        res['data'] = data
-        res['code'] = 1
-        res['msg'] = 'show course table'
-     
-    else: 
-        res['msg'] = 'unauthorized as student'
 
-    return HttpResponse(json.dumps(res),content_type = 'application/json')
-
-    pass
-
-
+##################################Check Infos#################################################
 def checkCourseTable_sql(request):
     res = {
         'code': 0,
@@ -407,28 +254,6 @@ def checkCourseTable_sql(request):
         res['data'] = data
         res['code'] = 1
         res['msg'] = 'show course table'
-     
-    else: 
-        res['msg'] = 'unauthorized as student'
-
-    return HttpResponse(json.dumps(res),content_type = 'application/json')
-
-# need to paginate
-def checkAllCourses(request):
-    res = {
-        'code': 0,
-        'msg': '',
-        'data':{}
-    }
-    # 0 - root 1 - students 2 -teachers
-    if request.session['is_login'] == True and request.session['role'] == STUDENT_ROLE:
-        user_id = request.GET['user_id']
-        print("the user id is ",user_id)
-        # the course name and credit hasn't been showed since the model may be changed later
-        data = serializers.serialize('python',models.Section.objects.all())
-        res['data'] = data
-        res['code'] = 1
-        res['msg'] = 'show all courses '
      
     else: 
         res['msg'] = 'unauthorized as student'
@@ -460,29 +285,6 @@ def checkAllCourses_sql(request):
         res['msg'] = 'unauthorized as student'
 
     return HttpResponse(json.dumps(res),content_type = 'application/json')
-
-# PAGE 5: for students to check his/her own info
-# def checkPersonalInfo(request):
-#     res = {
-#         'code': 0,
-#         'msg': '',
-#         'data':{}
-#     }
-
-#     # 0 - root 1 - students 2 -teachers
-#     if request.session['is_login'] == True and request.session['role'] == STUDENT_ROLE:
-#         user_id = request.GET['user_id']
-#         print("the user id is ",course_id)
-
-#         data = serializers.serialize('python',models.Student.objects.filter(student_id=user_id))
-#         res['data'] = data
-#         res['code'] = 1
-#         res['msg'] = 'show student info '
-     
-#     else: 
-#         res['msg'] = 'unauthorized as student'
-
-#     return HttpResponse(json.dumps(res,cls=DjangoJSONEncoder),content_type = 'application/json')
 
 def checkPersonalInfo_sql(request):
     res = {
@@ -518,8 +320,7 @@ def checkPersonalInfo_sql(request):
 
     return HttpResponse(json.dumps(res,cls=DjangoJSONEncoder),content_type = 'application/json')
 
-
-###################################################################################
+##################################Instructors: Check Course Name List#################################################
 # PAGE 6: for teachers to check the name list
 # PAY ATTENTION : have fixed the typo error in the table teaches (now  the instuctor_id be instructor_id )
 def checkTaughtCourses_sql(request):
@@ -544,7 +345,6 @@ def checkTaughtCourses_sql(request):
     else:
         res['msg'] = 'unauthorized as instructor'
     return HttpResponse(json.dumps(res,cls=DjangoJSONEncoder),content_type = 'application/json')
-
 
 def checkCourseNamelist(request):
     res = {
@@ -606,7 +406,7 @@ def checkCourseNamelist_sql(request):
 
     return HttpResponse(json.dumps(res,cls=DjangoJSONEncoder),content_type = 'application/json')
 
-####################Application without enough test!###############################################################
+####################Insturctors & Students :Application without enough test!###############################################################
 # PAGE 7: for teachers to handle the course applications
 def handleApplication_sql(request):
     res = {
@@ -748,8 +548,7 @@ def applyCourse_sql(request):
 
     return HttpResponse(json.dumps(res,cls=DjangoJSONEncoder),content_type = 'application/json')
 
-
-#############################Excel Processing###############################################################
+#############################Uploading and Excel Processing###############################################################
 # PAGE 8: for teachers to log the score of students in his class 
 def importExcel(res,request):
     if request.method == 'POST':
@@ -823,7 +622,7 @@ def searchCourse_sql(request):
 
 ######################################Exam Info#############################################################
 def checkExamInfo(request):
-    
+
     pass
 
 
